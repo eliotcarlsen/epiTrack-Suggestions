455c854d8917b648f382b951dbefb408f0d63ac7
Eliot Carlsen <eliot.carlsen@gmail.com>
2017-02-17 10:18:57 -0800

    add question 3 and pseudocode to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,31 @@
 $(document).ready(function(){
   $(".form").submit(function(event){
+
+
+
+
+
+    if 0-10 you might not like computer development, maybe try something else.
+    if question 1 and 5 >= 10 ---- Java
+    if question 2 and 6 >= 10 ---- PHP
+    if question 3 and 4 >= 10 ---- C#
+    if 30-35 you should take all the tracks!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     var userInputName = $("#userName").val();
     $(".name").empty().append(userInputName);
     event.preventDefault();
