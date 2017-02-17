bbfd14be2ba5b8a14e887a78bde156563d3a7a04
Eliot Carlsen <eliot.carlsen@gmail.com>
2017-02-17 10:33:40 -0800

    add values of variables in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,11 @@
 $(document).ready(function(){
   $(".form").submit(function(event){
-
-
-
-
-
-    if 0-10 you might not like computer development, maybe try something else.
-    if question 1 and 5 >= 10 ---- Java
-    if question 2 and 6 >= 10 ---- PHP
-    if question 3 and 4 >= 10 ---- C#
-    if 30-35 you should take all the tracks!
-
-
-
-
-
+    var question1 = parseInt($("input#question1").val());
+    var question2 = parseInt($("input#question2").val());
+    var question3 = parseInt($("input#question3").val());
+    var question4 = parseInt($("#question4").val());
+    var question5 = parseInt($("#question5").val());
+    var question6 = parseInt($("#question6").val());
 
 
 
