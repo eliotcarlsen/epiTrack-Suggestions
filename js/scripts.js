457308dfe81ed181527bdf02f058b90a9d16be70
Eliot Carlsen <eliot.carlsen@gmail.com>
2017-02-17 12:47:45 -0800

    add re-submit functionality to button by hiding previous entries

--- js/scripts.js
+++ js/scripts.js
@@ -12,18 +12,21 @@ $(document).ready(function(){
     var sumOfQ3Q4 = question3 + question4
     var sumOfAll = sumOfQ3Q4 + sumOfQ2Q6 + sumOfQ1Q5
     var userInputName = $("#userName").val();
-    console.log(userInputName);
-    console.log(sumOfAll);
+
     if (15 >= sumOfAll) {
+      $(".java, .php, .csharp").hide();
       $(".none").show();
       $(".name").empty().append(userInputName);
     } else if (10 <= sumOfQ1Q5) {
+      $(".none, .php, .csharp").hide();
       $(".java").show();
       $(".name").empty().append(userInputName);
     } else if (10 <= sumOfQ2Q6) {
+      $(".java, .none, .csharp").hide();
       $(".php").show();
       $(".name").empty().append(userInputName);
     } else if (10 <= sumOfQ3Q4) {
+      $(".java, .php, .none").hide();
       $(".csharp").show();
       $(".name").empty().append(userInputName);
     }
